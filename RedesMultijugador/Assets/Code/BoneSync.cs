75b1aba671879b66572ca8424d249f6a92c8cfb4
DP21-A2 <jquintana@cocoschool.mx>
2025-10-28 12:49:14 -0600

    Initial Commit

--- RedesMultijugador/Assets/Code/BoneSync.cs
+++ RedesMultijugador/Assets/Code/BoneSync.cs
@@ -1,43 +1,38 @@
 using UnityEngine;
 using Unity.Netcode;
 
-public class BoneSync : NetworkBehaviour
-{
+public class BoneSync : NetworkBehaviour {
     public Transform[] bones;
     public Transform[] targets;
 
     public TransformData[] transformDataArray;
 
 
-    private void Start()
-    {
+    private void Start() {
         transformDataArray = new TransformData[bones.Length];
+        Debug.LogWarning("data array: " + transformDataArray.Length);
     }
 
-    private void Update()
-    {
+    private void Update() {
         if (!IsOwner) return;
 
-        for (int i = 0; i < bones.Length; i++)
-        {
+        for (int i = 0; i < bones.Length; i++) {
             transformDataArray[i].Position = bones[i].position;
             transformDataArray[i].Rotation = bones[i].rotation;
         }
+        SendBoneArray_ServerRPC(transformDataArray);
     }
 
-    
+
 
     [ServerRpc]
-    void SendBoneArray_ServerRPC(TransformData[] data)
-    {
+    void SendBoneArray_ServerRPC(TransformData[] data) {
         ApplayTranformArrayToAll_ClientRPC(data);
     }
 
     [ClientRpc]
-    void ApplayTranformArrayToAll_ClientRPC(TransformData[] data)
-    {
-        for (int i = 0; i < data.Length; i++)
-        {
+    void ApplayTranformArrayToAll_ClientRPC(TransformData[] data) {
+        for (int i = 0; i < data.Length; i++) {
             targets[i].position = data[i].Position;
             targets[i].rotation = data[i].Rotation;
         }
--- RedesMultijugador/Assets/Code/TransformData.cs
+++ RedesMultijugador/Assets/Code/TransformData.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using Unity.Netcode;
 
-public class TransformData : INetworkSerializable
+public struct TransformData : INetworkSerializable
 {
     public Vector3 Position;
     public Quaternion Rotation;
