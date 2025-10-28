2e3af0993213643e45278a1a3cba978770e7cb7d
DP21-A2 <jquintana@cocoschool.mx>
2025-10-28 13:57:38 -0600

    Initial Commit

---
+++ RedesMultijugador/Assets/Code/HealthBar.cs
@@ -0,0 +1,26 @@
+using TMPro;
+using Unity.Netcode;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour {
+    [SerializeField, Range(0f, 100f)] NetworkVariable<float> m_health;
+    [SerializeField] TextMeshProUGUI m_textMeshPro;
+
+    private void Update() {
+
+    }
+
+    [ServerRpc]
+    void SendDamage_ServerRPC(float health, TextMeshProUGUI text, bool damagesetter) {
+
+    }
+
+    [ClientRpc]
+    void ApplyDamage_ServerRPC(float health, TextMeshProUGUI text, bool damagesetter) {
+        if (damagesetter) {
+            health--;
+        } else if (!damagesetter) {
+            health++;
+        }
+    }
+}
